f40858eac4dd46e79ce05b0fc47504fdcac89c97
Ramon Rodriguez <ramrodbej@alum.us.es>
2022-01-21 11:52:30 +0100

    commit 1

--- src/main/java/org/springframework/samples/petclinic/care/CareProvision.java
+++ src/main/java/org/springframework/samples/petclinic/care/CareProvision.java
@@ -1,5 +1,14 @@
 package org.springframework.samples.petclinic.care;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.samples.petclinic.pet.Visit;
 
 import lombok.Getter;
@@ -7,8 +16,20 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class CareProvision {   
+@Entity
+public class CareProvision {  
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer id;
+	
+	@NotNull
+	@Min(0)
     double duration;
+	
+	@ManyToOne(optional=true)
     Visit visit;
+    
+    @ManyToOne(optional=false)
+    @NotNull
     Care care;   
 }
--- src/main/java/org/springframework/samples/petclinic/care/CareService.java
+++ src/main/java/org/springframework/samples/petclinic/care/CareService.java
@@ -2,20 +2,28 @@ package org.springframework.samples.petclinic.care;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 
+@Service
 public class CareService {    
 
+	@Autowired
+	CareProvisionRepository careprovisionrepository;
+	
     public List<Care> getAllCares(){
         return null;
     }
 
     public List<Care> getAllCompatibleCares(String petTypeName){
-        return null;
+        List<Care> lista = careprovisionrepository.findCompatibleCares(petTypeName);
+        return lista;
     }
 
     public Care getCare(String careName) {
-        return null;
+        Care c = careprovisionrepository.findCareByName(careName);
+        return c;
     }
 
     
@@ -24,7 +32,8 @@ public class CareService {
     }
 
     public List<CareProvision> getAllCaresProvided(){
-        return null;
+        List<CareProvision> lista = careprovisionrepository.findAll();
+        return lista;
     }
 
     public List<CareProvision> getCaresProvided(Integer visitId){
